fe9b1b034925fccde341e81f8ce513ebe42ff413
evgeny <evgeny@ekurochkin>
2017-11-13 00:27:07 +0300

    test added

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,7 +8,7 @@ def test_init_storage_without_schema():
 
 
 def test_init_storage_with_schema():
-    schema = {
+    config = {
         'schema': {
             'data': {
                 'load': 'file_path',
@@ -19,8 +19,9 @@ def test_init_storage_with_schema():
                 'type': 'text'}
         }
     }
-    stor = SimpleStorage(".", schema=schema)
-    assert stor._schema == schema
+
+    stor = SimpleStorage(".", schema=config["schema"])
+    assert stor._schema == config["schema"]
 
 
 def test_read_empty(tmpdir):
@@ -28,9 +29,31 @@ def test_read_empty(tmpdir):
     assert not stor.read()
 
 
-def test_read_single_file(tmpdir):
-    f = tmpdir.mkdir("0001_TestData").join("0001_data.txt")
+def test_read_single_file_without_schema(tmpdir):
     text = "lala"
+
+    f = tmpdir.mkdir("0001_TestData").join("0001_data.txt")
     f.write("lala")
+
     stor = SimpleStorage(str(tmpdir))
-    assert stor.read()[0]["0001_data_txt"].text == text
+    item = stor.read()[0]
+    assert not item.tags
+    assert item.index == "0001_TestData"
+    assert item["0001_data_txt"].text == text
+
+
+def test_read_single_file_with_schema(tmpdir):
+    config = {
+        'schema': {
+            'data': {
+                'load': 'file_path',
+                'match': ['0001_data.*']}
+        }
+    }
+    text = "lala"
+
+    f = tmpdir.mkdir("0001_TestData").join("0001_data.txt")
+    f.write("lala")
+
+    stor = SimpleStorage(str(tmpdir), schema=config["schema"])
+    assert stor.read()[0]["data"].text == text
